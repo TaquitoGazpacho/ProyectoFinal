2367997f4fa444ee3622d7085f49d698ee47d979
gondhalf <gondhalf@git.com>
2018-01-15 10:37:59 +0100

    Condiciones progress bar

--- resources/views/user/pedidos.blade.php
+++ resources/views/user/pedidos.blade.php
@@ -23,8 +23,19 @@
                 <tr name = "ped">
                     <td id="pedido_{{$pedido->id}}_numero">{{$pedido->numero_pedido}}</td>
                     <td id="pedido_{{$pedido->id}}_estado">
+                        @php
+                            if($pedido->estado=="Enviado")
+                            {
+                                $p="60";
+                            }elseif($pedido->estado=="Recibido"){
+                                $p="80";
+
+                            }else{
+                                $p="100";
+                            }
+                        @endphp
                         <div class="progress">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: {{$p}}%;">
                                 {{$pedido->estado}}
                             </div>
                         </div>
@@ -36,15 +47,4 @@
             </tbody>
         </table>
     </div>
-
-@endsection
-
-@section('js')
-<script>
-    $( document ).ready( function() {
-        console.log($('tr[name=ped]'));
-
-
-    });
-</script>
 @endsection
