c6af1721fc66f518cc3e671ad8a2973da73b0a61
Andretxu6 <aespadamu16dw@ikzubirimanteo.com>
2018-01-19 12:55:06 +0100

    mapa creado, pone marcadores y localiza tu posición para centrar el mapa

--- public/js/mapUsuario.js
+++ public/js/mapUsuario.js
@@ -1,30 +1,59 @@
 //CREAR MAPA
 function getOficinasJson(){
+    //La funcion está definida en oficinas.blade.php
     var oficinas=getOficinasFromTemplate();
+
     var features=[];
+
     $(oficinas).each(function(index, oficina){
-        features.push({
+        var ofi={
             "type": "Feature",
-            "geometry": {
+            "geometry":
+            {
                 "type": "Point",
                 "coordinates": [oficina.alt, oficina.lat],
                 "longitud": oficina.lat,
                 "altitud": oficina.alt
             },
-            "properties": {
-                "title": oficina.calle+", "+oficina.num_calle,
+            "properties":
+            {
+                //"title": oficina.calle+", "+oficina.num_calle,
                 'marker-color': '#3bb2d0',
                 'marker-size': 'large',
                 'marker-symbol': 'rocket'
             }
-        });
+        };
+
+        var markerElement = document.createElement("i");
+        markerElement.className = "fa fa-3x fa-map-marker text-primary";
+
+        let marker = new mapboxgl.Marker(markerElement)
+            .setLngLat(ofi.geometry.coordinates)
+            .setPopup(
+                new mapboxgl.Popup({offset: 25})
+                    .setHTML("<h5>asdasdasdasdasdasdasdasd</h5>")
+            )
+            .addTo(map);
+
+        features.push(ofi);
     });
     return features
 }
+
+var posicion=[-3.70, 40.42]; //por defecto madrid
+//coge tus datos para la variable
+navigator.geolocation.getCurrentPosition(function (loc) {
+    posicion[0] = loc.coords.longitude;
+    posicion[1] = loc.coords.latitude;
+});
+
 mapboxgl.accessToken = 'pk.eyJ1IjoidGFxdWl0b2dhenBhY2hvIiwiYSI6ImNqY2xwMDU0OTA5b28zM3JvNG95OWZlM3kifQ.y-N-aKL1KQeSLJLY6C2mFg';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/mapbox/streets-v10',
+    renderWorldCopies: true,
+    center: posicion,
+    minZoom: 1.0
 });
 map.addControl(new mapboxgl.NavigationControl());
 map.on('load', function () {
--- resources/views/user/oficinas.blade.php
+++ resources/views/user/oficinas.blade.php
@@ -6,7 +6,7 @@
     @endphp
     <div class="row">
         <div class="col-md-12 col-xs-12 col-sm-12 col-lg-12">
-            <div id='map' style='width:1024px; height: 400px;'></div>
+            <div id='map' style="width:80%; min-height: 400px; max-height: 600px;"></div>
         </div>
     </div>
 @endsection
@@ -14,6 +14,7 @@
 @section('js')
     <script src="{{asset('js/mapUsuario.js')}}"></script>
     <script>
+        //La llamada se hace desde mapUsuario.js
         function getOficinasFromTemplate(){
             return @json($oficinas);
         }
