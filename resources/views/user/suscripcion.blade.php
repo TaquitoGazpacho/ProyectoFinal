822297fdaef9fdb2afaa33e0016a6cfc387241bd
iker <ikerruzo91@gmail.com>
2018-01-23 10:24:02 +0100

    Pricing de usuario cambiado y ajustados los estilos

--- resources/views/user/suscripcion.blade.php
+++ resources/views/user/suscripcion.blade.php
@@ -1,86 +1,55 @@
 @extends('user.userMaster')
 
 @section('contenido')
-    <div role="tabpanel" class="tab-pane" id="suscripcion">
-        <div class="container">
-            <div class="heading">
-                <h2 style="color: #F96F00;"><strong>Planes a gusto de todos</strong></h2>
-                <p>Nos adaptamos a tus necesidades</p>
-            </div><!-- //end heading -->
-            <form action="{{route('cambiarSuscripcion')}}" method="post">
-                {{ csrf_field() }}
-                <div class="row db-padding-btm db-attached">
-                    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                        <div class="db-wrapper">
-                            <div class="db-pricing-eleven db-bk-color-one">
-                                <div class="price">
-                                    10<sup>€</sup>
-                                    <small>anuales</small>
-                                </div>
-                                <div class="type">
-                                    Básico
-                                </div>
-                                <ul>
-
-                                    <li><i class="glyphicon glyphicon-print"></i>1 taquilla</li>
-                                    <li><i class="glyphicon glyphicon-time"></i>Usos ilimitados</li>
-                                    <li><i class="glyphicon glyphicon-trash"></i>Tamaño taquillas:<br>Pequeña y mediana</li>
-                                </ul>
-                                <div class="pricing-footer">
-
-                                    <input type="submit" name="button1" class="btn db-button-color-square btn-lg" value="Contrata Plan" />
-                                </div>
-                            </div>
-                        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-5 col-xs-10 col-xs-offset-1 col-sm-offset-1 col-lg-offset-0 col-md-offset-0">
+                <div class="pricingTable azul1">
+                    <div class="pricingTable-header">
+                        <span class="price-value">10<span class="currency">$</span></span>
+                        <h3 class="title">Standard</h3>
                     </div>
-                    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                        <div class="db-wrapper">
-                            <div class="db-pricing-eleven db-bk-color-two popular">
-                                <div class="price">
-                                    20<sup>€</sup>
-                                    <small>anuales</small>
-                                </div>
-                                <div class="type">
-                                    Premium
-                                </div>
-                                <ul>
-
-                                    <li><i class="glyphicon glyphicon-print"></i>3 taquilla</li>
-                                    <li><i class="glyphicon glyphicon-time"></i>Usos ilimitados</li>
-                                    <li><i class="glyphicon glyphicon-trash"></i>Tamaño taquillas:<br>Peuqeña, mediana y grande</li>
-                                </ul>
-                                <div class="pricing-footer">
-
-                                    <input type="submit" name="button2" class="btn db-button-color-square btn-lg" value="Contrata Plan" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                        <div class="db-wrapper">
-                            <div class="db-pricing-eleven db-bk-color-three">
-                                <div class="price">
-                                    150<sup>€</sup>
-                                    <small>anuales</small>
-                                </div>
-                                <div class="type">
-                                    Empresas
-                                </div>
-                                <ul>
-
-                                    <li><i class="glyphicon glyphicon-print"></i>10 taquillas</li>
-                                    <li><i class="glyphicon glyphicon-time"></i>Usos ilimitados</li>
-                                    <li><i class="glyphicon glyphicon-trash"></i>Tamaños extra aparte de los regulares, bajo necesidad.</li>
-                                </ul>
-                                <div class="pricing-footer">
-
-                                    <input type="submit" name="button3" class="btn db-button-color-square btn-lg" value="Contrata Plan" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <ul class="pricing-content">
+                        <li>50GB Disk Space</li>
+                        <li>50 Email Accounts</li>
+                        <li>50GB Monthly Bandwidth</li>
+                        <li>10 Subdomains</li>
+                        <li>15 Domains</li>
+                    </ul>
+                    <a href="#" class="pricingTable-signup">Sign Up</a>
                 </div>
-            </form>
-        </div><!-- FIN PRECIO PLANES -->
-    </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-5 col-xs-10 col-xs-offset-1 col-sm-offset-0 col-md-offset-0">
+                <div class="pricingTable azul2">
+                    <div class="pricingTable-header">
+                        <span class="price-value">20<span class="currency">$</span></span>
+                        <h3 class="title">Business</h3>
+                    </div>
+                    <ul class="pricing-content">
+                        <li>60GB Disk Space</li>
+                        <li>60 Email Accounts</li>
+                        <li>60GB Monthly Bandwidth</li>
+                        <li>15 Subdomains</li>
+                        <li>20 Domains</li>
+                    </ul>
+                    <a href="#" class="pricingTable-signup">Sign Up</a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-5 col-xs-10 col-xs-offset-1 col-sm-offset-1 col-lg-offset-0 col-md-offset-0">
+                <div class="pricingTable azul3">
+                    <div class="pricingTable-header">
+                        <span class="price-value">20<span class="currency">$</span></span>
+                        <h3 class="title">Business</h3>
+                    </div>
+                    <ul class="pricing-content">
+                        <li>60GB Disk Space</li>
+                        <li>60 Email Accounts</li>
+                        <li>60GB Monthly Bandwidth</li>
+                        <li>15 Subdomains</li>
+                        <li>20 Domains</li>
+                    </ul>
+                    <a href="#" class="pricingTable-signup">Sign Up</a>
+                </div>
+            </div>
+        </div>
+    {{--</div>--}}
 @endsection
