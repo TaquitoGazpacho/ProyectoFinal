41b0bc0a708986b824da67b5e3146bdd7588e379
Andretxu6 <aespadamu16dw@ikzubirimanteo.com>
2018-01-24 09:49:57 +0100

    empresa, modificada tabla de pedidos

--- resources/views/fijas/empresa.blade.php
+++ resources/views/fijas/empresa.blade.php
@@ -4,38 +4,65 @@
 
     $repartos = Reparto::getRepartos();
 @endphp
-@extends('fijas.master')
-
-@section('section')
-    <div class="panel panel-default">
+@extends('user.userMaster')
+@section('css')
+    <style>
+        #wrapper{
+            padding-left: 0px;
+        }
+    </style>
+@endsection
+@section('contenido')
+    <div>
         <!-- Default panel contents -->
-        <div class="panel-heading">Repartos de {{ Auth::guard('empresa')->user()->nombre }}</div>
+        <div>
+            <h3>Repartos de {{ Auth::guard('empresa')->user()->nombre }}</h3>
+        </div>
         <!-- Table -->
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Calle</th>
-                    <th>Ciudad</th>
-                    <th>Taquilla</th>
-                    <th>Cliente</th>
-                    <th>Clave Repartidor</th>
-                    <th>Clave Usuario</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($repartos as $reparto)
+        <div class="box-body table-responsive">
+            <table id="tablaEmpresa" class="table table-hover">
+                <thead>
                     <tr>
-                        <td>{{ DB::table('oficinas')->select('calle')->where('id', $reparto->oficina_id)->get()[0]->calle }}</td>
-                        <td>{{ DB::table('oficinas')->select('ciudad')->where('id', $reparto->oficina_id)->get()[0]->ciudad }}</td>
-                        <td>{{ DB::table('taquillas')->select('numero_taquilla')->where('id', $reparto->taquilla_id)->get()[0]->numero_taquilla }}</td>
-                        <td>{{ DB::table('users')->select('name', 'surname')->where('id', $reparto->usuario_id)->get()[0]->name }}
-                            {{ DB::table('users')->select('name', 'surname')->where('id', $reparto->usuario_id)->get()[0]->surname }}
-                        </td>
-                        <td>{{ $reparto->clave_repartidor }}</td>
-                        <td>{{ $reparto->clave_usuario }}</td>
+                        <th>Pais</th>
+                        <th>Direccion</th>
+                        <th>CP</th>
+                        <th>Taquilla</th>
+                        <th>Cliente</th>
+                        <th>Clave Repartidor</th>
+                        <th>Estado</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach($repartos as $reparto)
+                        <tr>
+                            <td>{{ $reparto->oficina->pais }}</td>
+                            <td>{{ $reparto->oficina->calle.", ".$reparto->oficina->num_calle }}</td>
+                            <td>{{ $reparto->oficina->cp }}</td>
+                            <td>{{ $reparto->taquilla->numero_taquilla }}</td>
+                            <td>{{ $reparto->usuario->name }}
+                                {{ "(".$reparto->usuario->phone.")" }}
+                            </td>
+                            <td>{{ $reparto->clave_repartidor }}</td>
+                            <td>{{ $reparto->estado }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
+@endsection
+
+@section('js')
+    <script>
+        $(function () {
+            $('#tablaEmpresa').DataTable({
+                'paging'      : true,
+                'lengthChange': false,
+                'searching'   : true,
+                'ordering'    : true,
+                'info'        : true,
+                'autoWidth'   : false
+            })
+        });
+    </script>
 @endsection
