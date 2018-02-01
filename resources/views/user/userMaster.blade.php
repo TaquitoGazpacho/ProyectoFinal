e20c2a165b3b83352875443998fea66c9da51eb8
Andretxu6 <aespadamu16dw@ikzubirimanteo.com>
2018-02-01 10:24:59 +0100

    active en el menu en tiendas relacionadas con lockbox

--- resources/views/user/userMaster.blade.php
+++ resources/views/user/userMaster.blade.php
@@ -64,7 +64,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('home.tiendas') }}">
+                            <a @if(\Request::route()->getName()=="home.tiendas") class="active" @endif href="{{ route('home.tiendas') }}">
                                 <span class="sidebar-icon"><i class="fa fa-shopping-bag"></i></span>
                                 <span class="sidebar-title">Tiendas asociadas</span>
                             </a>
