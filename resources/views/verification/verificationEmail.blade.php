7fda0496d4a589efe888db98ae706e343a5fe8c3
Andretxu6 <aespadamu16dw@ikzubirimanteo.com>
2018-02-01 11:36:04 +0100

    sidebar height

--- resources/views/verification/verificationEmail.blade.php
+++ resources/views/verification/verificationEmail.blade.php
@@ -1,5 +1,5 @@
 
-<body style="max-width: 500px; text-align: center; border: 1px solid #92BCE0; margin:auto">
+<body style="border-radius:10px; max-width: 500px; text-align: center; border: 1px solid #92BCE0; margin:auto">
     <h1 style="background: #3C8DBC; color: white; border-top: 2px solid #3071A9; border-bottom: 2px solid #3071A9">Bienvenido a Lock<span style="color: #F96F00">Box</span></h1>
     <h3 style="color: black">Estás a 1 paso de completar el registro</h3>
     <p style="color: black">Verifica el email con el boton que tienes a continuación</p>
