c67b6bb87ef83b0ff83d300cffc16211eccbc90f
iker <ikerruzo91@gmail.com>
2018-02-05 14:00:56 +0100

    Footer modificado

--- resources/views/fijas/footer.blade.php
+++ resources/views/fijas/footer.blade.php
@@ -1,27 +1,52 @@
-<footer id="mainFooter">
-    <div class="container-fluid fondo-footer">
+<footer id="footer">
+    <div class="row fondo-footer">
         <img src="{{ asset("img/footer.png") }}"/>
     </div>
-    <div class="footer-left">
-        <a class="vertical-logo-container" href="{{env('APP_URL')}}">
-            <img class="vertical-logo" src="{{asset('img/logo.png')}}">
-        </a>
-    </div>
-    <div class="footer-right">
-        <ul class="social-nav">
-            <li>Follow Us</li>
-            <li class="icon-bubble"><a href="#" target="_blank"><i class="fa fa-facebook"></i><span class="hidden">LockBox on Facebook</span></a></li>
-            <li class="icon-bubble"><a href="#" target="_blank"><i class="fa fa-twitter"></i><span class="hidden">LockBox on Twitter</span></a></li>
-            <li class="icon-bubble"><a href="#" target="_blank"><i class="fa fa-instagram"></i><span class="hidden">LockBox on Instagram</span></a></li>
-            <li class="icon-bubble"><a href="#" target="_blank"><i class="fa fa-github"></i><span class="hidden">LockBox on Instagram</span></a></li>
-        </ul>
-        <ul class="legal-nav">
-            <li><a href="#">About Us</a></li>
-            <li><a href="#" target="_blank">Ad Choices</a></li>
-            <li><a href="#" target="_blank">Terms Of Service</a></li>
-            <li><a href="#" target="_blank">Contact Us</a></li>
-            <li><a href="#" target="_blank">Privacy Policy</a></li>
-            <li>© 2018 LockBox</li>
-        </ul>
+    <div class="row row-eq-height">
+        <div class="col-xs-12 col-md-4 col-lg-4 text-center">
+            <a class="vertical-logo-container" href="{{env('APP_URL')}}">
+                <img class="vertical-logo" src="{{asset('img/logo.png')}}">
+            </a>
+        </div>
+        <div class="col-xs-12 col-md-8 col-lg-8">
+            <div class="row">
+                <ul class="social-nav">
+                    <div class="col-xs-4 col-md-2 text-center">
+                        <li class="icon-bubble"><a href="#" target="_blank"><i class="fa fa-facebook"></i><span class="hidden">LockBox on Facebook</span></a></li>
+                    </div>
+                    <div class="col-xs-4 col-md-2 text-center">
+                        <li class="icon-bubble"><a href="#" target="_blank"><i class="fa fa-twitter"></i><span class="hidden">LockBox on Twitter</span></a></li>
+                    </div>
+                    <div class="col-xs-4 col-md-2 text-center">
+                        <li class="icon-bubble"><a href="#" target="_blank"><i class="fa fa-instagram"></i><span class="hidden">LockBox on Instagram</span></a></li>
+                    </div>
+                    <div class="col-xs-4 col-md-2 text-center">
+                        <li class="icon-bubble"><a href="#" target="_blank"><i class="fa fa-github"></i><span class="hidden">LockBox on Instagram</span></a></li>
+                    </div>
+                    <div class="col-xs-4 col-md-2 text-center">
+                        <li class="icon-bubble"><a href="#" target="_blank"><i class="fa fa-tumblr"></i><span class="hidden">LockBox on Instagram</span></a></li>
+                    </div>
+                    <div class="col-xs-4 col-md-2 text-center">
+                        <li class="icon-bubble"><a href="#" target="_blank"><i class="fa fa-google-plus"></i><span class="hidden">LockBox on Instagram</span></a></li>
+                    </div>
+                </ul>
+            </div>
+            <div class="row row-legal-nav">
+                <ul class="legal-nav">
+                    <div class="col-xs-4 text-center">
+                        <li><a href="#">Sobre nosotros</a></li>
+                    </div>
+                    <div class="col-xs-4 text-center">
+                        <li><a href="#" target="_blank">Contáctanos</a></li>
+                    </div>
+                    <div class="col-xs-4 text-center">
+                        <li><a href="#" target="_blank">Términos de uso</a></li>
+                    </div>
+                    <div class="col-xs-12 text-center nombre">
+                        <li>© 2018 LockBox</li>
+                    </div>
+                </ul>
+            </div>
+        </div>
     </div>
 </footer>
