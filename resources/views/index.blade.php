19bb78f2941075b35cdf68c20c23676dd23fb9be
Andretxu6 <aespadamu16dw@ikzubirimanteo.com>
2018-02-21 12:02:07 +0100

    Cambios de última hora

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -36,14 +36,7 @@
                 var header = document.getElementsByTagName('header')[0];
                 var nav = document.getElementById('nav');
                 var navHeight = parseInt(window.getComputedStyle(nav, null).getPropertyValue('height'));
-                var html = document.getElementsByTagName('html')[0];
                 header.style.height = parseInt(window.innerHeight) - navHeight + 'px';
-                // if (document.URL.includes("login") || document.URL.includes("register")) {
-                //     //document.body.style.height = parseInt(window.innerHeight) + 'px';
-                //     //html.style.height = parseInt(window.innerHeight) + 'px';
-                //     document.body.style.overflow = 'hidden';
-                // }
-                addEvent();
             } catch (e) {}
         }
 
